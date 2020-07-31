b1bf00085a1f9c2ccd8b8dd82134dc299d14a533
Adrian Tosca <adrian.tosca@tradeshift.com>
2020-07-31 20:18:26 +0300

    fix multidex app

--- androidapp/app/src/main/java/ro/cipcirip/CipCiripApplication.kt
+++ androidapp/app/src/main/java/ro/cipcirip/CipCiripApplication.kt
@@ -1,11 +1,11 @@
 package ro.cipcirip
 
-import android.app.Application
+import androidx.multidex.MultiDexApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
-class CipCiripApplication: Application() {
+class CipCiripApplication: MultiDexApplication() {
     override fun onCreate(){
         super.onCreate()
         startKoin {
